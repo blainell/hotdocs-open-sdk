e37eaab721253d4e334e5d60e7a5afad9cfc3862
jcannegieter <jared.cannegieter@hotdocs.com>
2013-09-12 14:51:13 -0600

    More XML comment placeholders.

--- HotDocs.Sdk/PackageTemplateLocation.cs
+++ HotDocs.Sdk/PackageTemplateLocation.cs
@@ -10,16 +10,35 @@ using System.IO;
 
 namespace HotDocs.Sdk
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	public abstract class PackageTemplateLocation : TemplateLocation
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="packageID"></param>
 		public PackageTemplateLocation(string packageID)
 		{
 			PackageID = packageID;
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
 		public abstract Stream GetPackageStream();
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
 		public abstract TemplatePackageManifest GetPackageManifest();
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public string PackageID { get; protected set; }
 	}
 }
