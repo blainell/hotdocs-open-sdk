588118574b7c57297d61ce2731194a4eeaa4bf3e
klwilkins <kevinleewilkins@gmail.com>
2013-09-30 10:07:40 -0600

    Removed superfluous "template" parameter from RestClientSession.ResumeSession.

--- HotDocs.Sdk.Cloud/Rest/RestClientSession.cs
+++ HotDocs.Sdk.Cloud/Rest/RestClientSession.cs
@@ -70,7 +70,7 @@ namespace HotDocs.Sdk.Cloud
 		/// <param name="template">The name of the template package for which a bookmark was saved.</param>
 		/// <param name="state">The serialized state.</param>
 		/// <returns>The URL to use in the src attribute of the iframe.</returns>
-		public string ResumeSession(string template, string state)
+		public string ResumeSession(string state)
 		{
 			var timestamp = DateTime.UtcNow;
 
