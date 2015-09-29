6895ee5d2c640a87b97d9433c73a9bf6b13f50d6
Ruaidhri Primrose <ruaidhri.primrose@hotdocs.co.uk>
2015-09-29 14:51:40 +0100

    Initial commit of NuGet upload scripts

---
+++ BuildScripts/BuildFunctions.psm1
@@ -0,0 +1,109 @@
+function Publish-NugetPackage
+{
+  Param
+  (
+    [string]$SrcPath,
+    [string]$NugetPath,
+    [string]$PackageVersion,
+    [string]$NugetServer,
+    [string]$NugetServerPassword
+  )
+    if (!$SrcPath){
+    throw "No source path specified"
+    }
+    if (!$NugetPath){
+    throw "No NugetPath specified"
+    }
+    if (!$PackageVersion){
+    throw "No PackageVersion specified"
+    }
+    if (!$NugetServer){
+    throw "No NugetServer specified"
+    }
+    if (!$NugetServerPassword){
+    throw "No NugetServerPassword specified"
+    }
+
+    Write-Host "Executing Publish-NugetPackage in path $SrcPath, PackageVersion is $PackageVersion"
+
+    $AllNuspecFiles = Get-ChildItem $SrcPath\*.nuspec
+    Write-Host "all files is $AllNuspecFiles"
+    #Remove all previous packed packages in the directory
+    $AllNugetPackageFiles = Get-ChildItem $SrcPath\*.nupkg
+
+    Write-Host "Removing old nupkg files"
+    foreach ($file in $AllNugetPackageFiles)
+    {
+	    Write-Host "Removing file $file"
+      Remove-Item $file
+    }
+
+    Write-Host "Creating new nupkg files"
+
+    foreach ($file in $AllNuspecFiles)
+    {
+        Write-Host "Modifying file " + $file.FullName
+        #save the file for restore
+        $backFile = $file.FullName + "._ORI"
+        $tempFile = $file.FullName + ".tmp"
+        Copy-Item $file.FullName $backFile -Force
+
+        #now load all content of the original file and rewrite modified to the same file
+        Get-Content $file.FullName |
+        %{$_ -replace '<version>[0-9]+(\.([0-9]+|\*)){1,3}</version>', "<version>$PackageVersion</version>" } > $tempFile
+        Move-Item $tempFile $file.FullName -force
+
+        #Create the .nupkg from the nuspec file
+        $ps = new-object System.Diagnostics.Process
+        $ps.StartInfo.Filename = "$NugetPath\nuget.exe"
+        $ps.StartInfo.Arguments = "pack `"$file`""
+        $ps.StartInfo.WorkingDirectory = $file.Directory.FullName
+        $ps.StartInfo.RedirectStandardOutput = $True
+        $ps.StartInfo.RedirectStandardError = $True
+        $ps.StartInfo.UseShellExecute = $false
+        $ps.start()
+        if(!$ps.WaitForExit(30000))
+        {
+            $ps.Kill()
+        }
+        [string] $Out = $ps.StandardOutput.ReadToEnd();
+        [string] $ErrOut = $ps.StandardError.ReadToEnd();
+        Write-Host "Nuget pack Output of commandline " + $ps.StartInfo.Filename + " " + $ps.StartInfo.Arguments
+        Write-Host $Out
+        if ($ErrOut -ne "")
+        {
+            Write-Error "Nuget pack Errors"
+            Write-Error $ErrOut
+        }
+    }
+
+    $AllNugetPackageFiles = Get-ChildItem $SrcPath\*.nupkg
+
+    Write-Host "Uploading NuPkg files to server"
+    foreach ($file in $AllNugetPackageFiles)
+    {
+        #Create the .nupkg from the nuspec file
+        $ps = new-object System.Diagnostics.Process
+        $ps.StartInfo.Filename = "$NugetPath\nuget.exe"
+        $ps.StartInfo.Arguments = "push `"$file`" -s $NugetServer $NugetServerPassword"
+        $ps.StartInfo.WorkingDirectory = $file.Directory.FullName
+        $ps.StartInfo.RedirectStandardOutput = $True
+        $ps.StartInfo.RedirectStandardError = $True
+        $ps.StartInfo.UseShellExecute = $false
+        $ps.start()
+        if(!$ps.WaitForExit(30000))
+        {
+            $ps.Kill()
+        }
+        [string] $Out = $ps.StandardOutput.ReadToEnd();
+        [string] $ErrOut = $ps.StandardError.ReadToEnd();
+        Write-Host "Nuget push Output of commandline " + $ps.StartInfo.Filename + " " + $ps.StartInfo.Arguments
+        Write-Host $Out
+        if ($ErrOut -ne "")
+        {
+            Write-Error "Nuget push Errors"
+            Write-Error $ErrOut
+        }
+
+    }
+}
---
+++ BuildScripts/UploadNugetPackage.ps1
@@ -0,0 +1,42 @@
+Param
+(
+[string] $NugetServer = "\\edv06nuget01\nuget\packages",
+[string] $NugetServerPassword = " "
+)
+
+if (!(Test-Path Env:\GO_PIPELINE_LABEL)){
+  throw "GO_PIPELINE_LABEL does not exist"
+}
+if (!(Test-Path Env:\MAJOR_VERSION)){
+  throw "MAJOR_VERSION does not exist"
+}
+if (!(Test-Path Env:\MINOR_VERSION)){
+  throw "MINOR_VERSION does not exist"
+}
+if (!(Test-Path Env:\NUGET_SERVER)){
+  throw "NUGET_SERVER does not exist"
+}
+if (!(Test-Path Env:\NUGET_SERVER_PASSWORD)){
+  throw "NUGET_SERVER_PASSWORD does not exist"
+}
+
+$BuildNumber = (Get-Item Env:\GO_PIPELINE_LABEL).Value
+$MajorVersion = (Get-Item Env:\MAJOR_VERSION).Value
+$MinorVersion = (Get-Item Env:\MINOR_VERSION).Value
+$NugetServer = (Get-Item Env:\NUGET_SERVER).Value
+$NugetServerPassword = (Get-Item Env:\NUGET_SERVER_PASSWORD).Value
+
+$packageVersion = "$MajorVersion.$MinorVersion.$BuildNumber.0"
+
+$scriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+Write-Host "Script root is $scriptRoot"
+
+Import-Module $scriptRoot\BuildFunctions
+
+$binPath = "..\BuildArtifacts"
+Write-Host "binPath is $binPath"
+Write-Host "scriptRoot is $scriptRoot"
+Write-Host "PackageVersion is $PackageVersion"
+Write-Host "NuGet server is $NugetServer"
+
+Publish-NugetPackage -SrcPath $binPath -NuGetPath $scriptRoot -PackageVersion $PackageVersion -NuGetServer $NugetServer -NugetServerPassword $NugetServerPassword
