eaf84a555f034e4e6698d20f3239b1b5d71d10ce
Ruaidhri Primrose <ruaidhri.primrose@hotdocs.co.uk>
2015-11-09 17:06:27 +0000

    Amend build script to gather output to SonarQube

--- BuildScripts/BuildAndUnitTest.ps1
+++ BuildScripts/BuildAndUnitTest.ps1
@@ -1,6 +1,15 @@
 $ErrorActionPreference = "Stop"
+$env:Path += ";C:\Program Files (x86)\WiX Toolset v3.10\bin"
+$location = (Get-Item -Path ".\" -Verbose).FullName
+	
+if ($env:GO_ENVIRONMENT_NAME){
+	C:\SonarQube\MSBuild.SonarQube.Runner-1.01\MSBuild.SonarQube.Runner.exe begin /k:$env:SONAR_PROJECT_KEY /n:$env:SONAR_PROJECT_NAME /v:$env:SONAR_PROJECT_VERSION
+}
 	
 ./NuGet.exe restore ..\HotDocs.Sdk.sln
 msbuild /t:"Build" BuildAndUnitTest.build
 
+if ($env:GO_ENVIRONMENT_NAME){			
+	C:\SonarQube\MSBuild.SonarQube.Runner-1.01\MSBuild.SonarQube.Runner.exe end
+}	
 exit $LastExitCode
